80a2d52056bd1105ffa0e74d50f021f5ff7e7e99
MrPeep517 <mrpeep0517@gmail.com>
2024-05-19 16:16:18 -0700

    Made the help option to just ask the user to visit the documentation website.

--- __main__.py
+++ __main__.py
@@ -10,17 +10,4 @@ while run:
 
     match cmd:
         case "help":
-            while True:
-                specify = input("What would documentation would you like to see specificly. (Type all to veiw all and type nothing to go back.) ").lower()
-                if specify == "all":
-                    print(" ".join(help_menu))
-                    break
-                elif specify == "":
-                    break
-                else:
-                    try:
-                        print(help_menu[specify])
-                        break
-                    except:
-                        print("There is no section on that. Please try again")
-                        continue
+            print("For help please go to ")
